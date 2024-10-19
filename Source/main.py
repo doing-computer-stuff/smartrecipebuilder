fb8a298bf8bd1e954fd083710ee7e0d9c1e966fd
GeneralMills31 <115842007+GeneralMills31@users.noreply.github.com>
2024-10-19 18:07:02 -0500

    Update main.py

--- Source/main.py
+++ Source/main.py
@@ -1,8 +1,8 @@
 import mysql.connector
 from login import show_login_screen
-#import mysql.connector
+import mysql.connector
 
 if __name__ == "__main__":
-    #db_conn = mysql.connector.connect(host="localhost", user="root", passwd="ics311", database="library")
-    show_login_screen()
+    db_conn = mysql.connector.connect(host="localhost", user="root", passwd="Development1234", database="smartrecipebuilder")
+    show_login_screen(db_conn)
     #db_conn.close()
