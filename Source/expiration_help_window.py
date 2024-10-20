086adb77b7f13c725178e418e90234cbc605d720
doing-computer-stuff <118572359+doing-computer-stuff@users.noreply.github.com>
2024-10-20 11:52:51 -0500

    update expiration date help screen

--- Source/expiration_help_window.py
+++ Source/expiration_help_window.py
@@ -6,33 +6,39 @@ def show_expiration_help_window():
 
     # Create the window
     window = tk.Tk()
-    window.geometry("400x400")
-    window.configure(bg="white")
+    window.geometry("300x250")
+    window.configure(bg="#4EB276")
     window.title("Expiration Date Guidelines")
     window.resizable(False, False)
 
+    # Define Table Styling
+    style = ttk.Style(window)
+    style.theme_use("clam")
+    style.configure("Treeview", background="#D9D9D9", fieldbackground="#D9D9D9", borderdwidth=0, relief="flat")
+    style.map('Treeview', background=[('selected', '#284846')])
+
     # Create the Treeview
-    table = ttk.Treeview(window, columns=("Column 1", "Column 2"), show="headings")
+    table = ttk.Treeview(window, columns=("Column 1", "Column 2"), show="headings", style="Treeview")
 
     # Define the headings
     table.heading("Column 1", text="Product")
+    table.column("Column 1", width="150", anchor="c")
     table.heading("Column 2", text="Shelf Life")
+    table.column("Column 2", width="150", anchor="c")
 
     # Insert some grocery data
-    table.insert("", tk.END, values=("Bread", "1 Week"))
-    table.insert("", tk.END, values=("Flour", "12 Months"))
-    table.insert("", tk.END, values=("Canned Goods", "2-5 Years"))
-    table.insert("", tk.END, values=("Butter", "1-3 Months"))
-    table.insert("", tk.END, values=("Cheese", "3-4 Weeks"))
-    table.insert("", tk.END, values=("Canned Goods", "2-5 Years"))
-    table.insert("", tk.END, values=("Eggs", "3-5 Weeks"))
-    table.insert("", tk.END, values=("Apple", "1-2 Months"))
-    table.insert("", tk.END, values=("Carrots", "1-2 Months"))
-    table.insert("", tk.END, values=("Onions", "1-3 Months"))
-    table.insert("", tk.END, values=("Bananas", "1 Week"))
+    table.insert("", tk.END, values=("Tomatoes", "5-7 Days"))
+    table.insert("", tk.END, values=("Leafy Greens", "10-12 Days"))
+    table.insert("", tk.END, values=("Celery", "14-21 Days"))
+    table.insert("", tk.END, values=("Avocados", "7-14 Days"))
+    table.insert("", tk.END, values=("Apples", "30 Days"))
+    table.insert("", tk.END, values=("Oranges", "14 Days"))
+    table.insert("", tk.END, values=("Strawberries", "3-5 Days"))
+    table.insert("", tk.END, values=("Watermelon", "6-8 Days"))
+    table.insert("", tk.END, values=("Bananas", "5-7 Days"))
+    table.insert("", tk.END, values=("Onions", "60-90 Days"))
+    table.insert("", tk.END, values=("Mushrooms", "7-8 Days"))
+    table.insert("", tk.END, values=("Potatoes", "21-35 Days"))
 
     # Pack the table
-    table.pack()
-
-    # Run the main loop
-    window.mainloop()
+    table.pack(expand=1)
