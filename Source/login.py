d799d059c00cb3a9a77aad10fdac2db25fe1d5dd
GeneralMills31 <115842007+GeneralMills31@users.noreply.github.com>
2024-10-21 19:53:18 -0500

    Fixed login errors

--- Source/login.py
+++ Source/login.py
@@ -127,44 +127,6 @@ def show_login_screen(db_conn):
         height=39.0
     )
 
-    # Method to check whether the given username and password are in the database.
-    # Returns true if there is at least one row with the given username AND password.
-    def verify_login_credentials(username, password):
-
-        # Create a cursor to execute SQL commands.
-        cursor = db_conn.cursor(dictionary = True)
-
-        # Execute query to see whether user exists.
-        cursor.execute(
-        "SELECT user_password FROM users WHERE username = %s",
-        (username,))
-
-        # Under assumption usernames are unique!
-        db_password = cursor.fetchall()[0]["user_password"]
-        
-        # Commit changes to the database and close connection to cursor.
-        cursor.close()
-
-        return compare_passwords(db_password, password)
-
-    # Method to check whether the given username and password are in the database.
-    def get_user_info(username):
-        # Create a cursor to execute SQL commands.
-        cursor = db_conn.cursor(dictionary=True)
-
-        # Execute query to see whether user exists.
-        cursor.execute(f"SELECT username, user_id FROM users WHERE username = '{username}'")
-
-        # Grab the id of the user
-        # This could cause issues or incorrect behavior if 
-        # usernames are not unique
-        result = cursor.fetchone()
-
-        # Commit changes to the database and close connection to cursor.
-        cursor.close()
-
-        return result
-
 
     # links and functions for create account and reset password
     def navigate_to_create_account():
@@ -189,25 +151,56 @@ def show_login_screen(db_conn):
     # login button and function
     def login():
         from home_screen import show_home_screen
+
+        def valid_username(username):
+            # Verify that the username exists in the db.
+            cursor = db_conn.cursor()
+            cursor.execute(f"SELECT COUNT(*) FROM users WHERE username = '{username}';")
+            result = cursor.fetchone()
+            cursor.close()
+            if result[0] == 0:
+                return False
+            return True
+
+        # Method to check whether the given username and password are in the database.
+        # Returns true if there is at least one row with the given username AND password.
+        def verify_login_credentials(username, password):
+            cursor = db_conn.cursor(dictionary = True)
+            # Execute query to see whether user exists.
+            cursor.execute(
+            "SELECT user_password FROM users WHERE username = %s",
+            (username,))
+            # Under assumption usernames are unique!
+            db_password = cursor.fetchall()[0]["user_password"]
+            cursor.close()
+            return compare_passwords(db_password, password)
+
+            # Method to check whether the given username and password are in the database.
+        def get_user_info(username):
+            cursor = db_conn.cursor(dictionary=True)
+            # Execute query to see whether user exists.
+            cursor.execute(f"SELECT username, user_id FROM users WHERE username = '{username}'")
+            result = cursor.fetchone()
+            cursor.close()
+            return result
+
         # Just placeholder code, the user_info passed to home_screen will be in associative array form already, returns that way from db query.
         username = username_input.get()
         password = password_input.get()
 
-        # Check if username and password are in user database.
-        # If all is good, connect to the database, get the users unique id, and send them to the homescreen.
-        if verify_login_credentials(username, password):
+        if valid_username(username):
+            if verify_login_credentials(username, password):
+                user_info = get_user_info(username)
+                window.destroy()
+                # Pass the database connection and user ID here.
+                show_home_screen(db_conn, user_info['username'], user_info['user_id'])
+            else:
+                messagebox.showwarning("Invalid Credentials", "Incorrect Password.")
+                return
 
-            # Use the user database to get the users unique ID
-            # and store it here.
-            user_info = get_user_info(username)
-
-            window.destroy()
-
-            # Pass the database connection and user ID here.
-            show_home_screen(db_conn, user_info['username'], user_info['user_id'])
         else:
-            messagebox.showwarning("Invalid Credentials", "Incorrect username or password.")
-            print("log in with 'user' and 'password'")
+            messagebox.showwarning("Invalid Credentials", "Username does not exist.")
+            return
 
     login_button = Button(
         image=button_image_1,
