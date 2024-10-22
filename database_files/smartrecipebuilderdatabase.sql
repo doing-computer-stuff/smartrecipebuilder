417b39fd502c125d951fdba68a123690a2ca86fa
GeneralMills31 <115842007+GeneralMills31@users.noreply.github.com>
2024-10-22 16:36:15 -0500

    Updated quantity, food_id, and password length.

--- database_files/smartrecipebuilderdatabase.sql
+++ database_files/smartrecipebuilderdatabase.sql
@@ -27,11 +27,14 @@ DROP TABLE IF EXISTS `ingredients`;
 CREATE TABLE `ingredients` (
   `food_name` varchar(50) DEFAULT NULL,
   `expiration_date` date DEFAULT NULL,
-  `quantity` int DEFAULT NULL,
+  `quantity` varchar(15) DEFAULT NULL,
   `user_id` int DEFAULT NULL,
+  `food_id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`food_id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `ingredients_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `ingredients_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `ingredients_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +43,7 @@ CREATE TABLE `ingredients` (
 
 LOCK TABLES `ingredients` WRITE;
 /*!40000 ALTER TABLE `ingredients` DISABLE KEYS */;
+INSERT INTO `ingredients` VALUES ('Apples','2024-10-15','6',1,1),('Carrots','2024-11-22','10',1,2),('Bananas','2024-12-01','3',2,3),('Pizza','2024-10-22','1 Slice',1,7),('Pineapple','2024-11-21','1 Fruit',1,8);
 /*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,13 +56,11 @@ DROP TABLE IF EXISTS `users`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `user_id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50) DEFAULT NULL,
-  `last_name` varchar(50) DEFAULT NULL,
   `account_creation_date` date DEFAULT NULL,
   `username` varchar(50) DEFAULT NULL,
-  `user_password` varchar(50) DEFAULT NULL,
+  `user_password` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +69,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,NULL,NULL,NULL,'user1','password1'),(2,NULL,NULL,NULL,'user2','password2');
+INSERT INTO `users` VALUES (1,NULL,'user1','0b14d501a594442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e'),(2,NULL,'user2','password2'),(3,NULL,'user3','password3'),(4,NULL,'user4','password4'),(5,NULL,'user5','password5'),(6,NULL,'user7','password7'),(7,NULL,'user8','password8'),(8,NULL,'user9','password9'),(9,NULL,'user10','aa4a9ea03fcac15b5fc63c949ac34e7b0fd17906716ac3b8e58c599cdc5a52f0'),(10,NULL,'user11','53d453b0c08b6b38ae91515dc88d25fbecdd1d6001f022419629df844f8ba433'),(11,NULL,'user12','b3d17ebbe4f2b75d27b6309cfaae1487b667301a73951e7d523a039cd2dfe110'),(12,NULL,'user20','7535d8f2d8c35d958995610f971287288ab5e8c82a3c4fdc2b6fb5d757a5b9f8');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -80,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-14 17:24:41
+-- Dump completed on 2024-10-22 16:29:35
