5edd2f1bd95777d8bc9fd0a08e912f9420562742
GeneralMills31 <115842007+GeneralMills31@users.noreply.github.com>
2024-11-11 18:06:22 -0600

    Updates for SQLite

--- Source/main.py
+++ Source/main.py
@@ -1,7 +1,14 @@
 import mysql.connector
 from login import show_login_screen
+from utilities import *
+import sqlite3
+import os
 
 if __name__ == "__main__":
-    db_conn = mysql.connector.connect(host="localhost", user="root", passwd="ics311", database="smartrecipebuilder")
+    # Path is in the same location as the program.
+    db_path = "smartrecipebuilder.db"
+    # Connect to database if it exists. If it does not, create it.
+    db_conn = create_database_if_not_exists(db_path)
+    # print("Current working directory:", os.getcwd())
     show_login_screen(db_conn)
     db_conn.close()
