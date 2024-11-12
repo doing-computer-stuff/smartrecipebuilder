cb759b3091d499e9b2226d868d423b23832f7cce
GeneralMills31 <115842007+GeneralMills31@users.noreply.github.com>
2024-11-11 18:10:01 -0600

    Updates for SQLite

--- Source/utilities.py
+++ Source/utilities.py
@@ -3,6 +3,8 @@ from tkinter import *
 from tkinter import messagebox
 import mysql.connector
 import hashlib
+import sqlite3
+import os
 
 def hash_password(password):
     # Hash the password before storing into database.
@@ -16,3 +18,42 @@ def compare_passwords(db_password, entered_password):
     hashed_password = hashlib.sha256(entered_password.encode()).hexdigest()
 
     return db_password == hashed_password
+
+# Method that check if the sqlite database exists. If it does, it connects
+# to the database and returns the connection. If it does not, it create the
+# database and initalizes the tables.
+def create_database_if_not_exists(db_path):
+    # Check if the database file already exists.
+    if not os.path.exists(db_path):
+        # Connect to the database, creating it if it doesn't exist.
+        with sqlite3.connect(db_path) as conn:
+            cursor = conn.cursor()
+            
+            # Create users table.
+            cursor.execute('''
+                CREATE TABLE users (
+                    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    account_creation_date text DEFAULT NULL,
+                    username varchar(50) DEFAULT NULL,
+                    user_password varchar(64) DEFAULT NULL
+                )
+            ''')
+
+            # Create ingredients table.
+            cursor.execute('''
+                CREATE TABLE ingredients (
+                    food_name varchar(50) DEFAULT NULL,
+                    expiration_date text DEFAULT NULL,
+                    quantity varchar(15) DEFAULT NULL,
+                    user_id INTEGER DEFAULT NULL,
+                    food_id INTEGER PRIMARY KEY NOT NULL,
+                    FOREIGN KEY (user_id) REFERENCES users(user_id)
+                )
+            ''')
+            
+            # Commit changes
+            conn.commit()
+            return conn
+    else:
+        conn = sqlite3.connect(db_path)
+        return conn
