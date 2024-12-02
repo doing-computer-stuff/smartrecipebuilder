4a8f669fccb4c41de13291a41e935bc71dbbeee6
GeneralMills31 <115842007+GeneralMills31@users.noreply.github.com>
2024-12-01 18:08:24 -0600

    Added Pep 257 Comments

--- Source/main.py
+++ Source/main.py
@@ -1,10 +1,12 @@
+"""Main module for the system.
+"""
 from login import show_login_screen
 from utilities import *
 
 if __name__ == "__main__":
-    # Path is in the same location as the program.
+    """Path is in the same location as the program."""
     db_path = "smartrecipebuilder.db"
-    # Connect to database if it exists. If it does not, create it.
+    """Connect to database if it exists. If it does not, create it."""
     db_conn = create_database_if_not_exists(db_path)
     show_login_screen(db_conn)
     db_conn.close()
